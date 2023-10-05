4dcec24e429668e451342af2f06b7a038b7f4c99
Darla Souza <darlan.as@outlook.com>
2023-10-05 19:54:37 -0300

    Exercicios 23 e 24 finalizados

--- Exercícios/ex024.py
+++ Exercícios/ex024.py
@@ -1,7 +1,5 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome 'Santo'.
 
-cid = str(input('Digite o nome da sua cidade: '))
-cid0 = cid.strip()
-cid1 = cid0.capitalize()
-cid2 = 'Santo' in cid1
-print(cid2)
+cid = str(input('Digite o nome da sua cidade: ')).strip().capitalize()
+cid1 = 'Santo' in cid
+print(cid1)
