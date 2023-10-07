f0f6e6247e3cdf7296b1a2f5413b2fcd0964d342
Darla Souza <darlan.as@outlook.com>
2023-10-07 14:39:24 -0300

    exercicio 28 realizado

--- Exercícios/ex028.py
+++ Exercícios/ex028.py
@@ -1,3 +1,17 @@
 # Escreva um programa que faça o pc 'pensar' em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir
 # qual foi o número escolhido pelo pc.
 # O programa deverá escrever na tela se o usuário vendeu ou perdeu.
+
+from random import randint
+from time import sleep
+pc = randint(0, 5)  # Faz o pc 'pensar'
+print('-=-' * 18)
+print('Vou pensar em um número entre 0 e 5, tente adivinhar!')
+print('-=-' * 18)
+jogador = int(input('Em que número pensei? '))  # jogador tenta adivinhar
+print('Processando...')
+sleep(1)
+if jogador == pc:
+    print('Parabéns, você venceu!!!')
+else:
+    print('Ganhei, pensei no número {} e não {}!'.format(pc, jogador))
