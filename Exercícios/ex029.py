01b3e27a89f814f499a20b92190423ee2b673e14
Darla Souza <144633945+Darlan03@users.noreply.github.com>
2023-10-07 23:08:13 -0300

    Update ex029.py
    Exercício 29 realizado

--- Exercícios/ex029.py
+++ Exercícios/ex029.py
@@ -2,3 +2,18 @@
 # Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
 # A multa vai custar R$ 7,00 por cada Km acima do limite.
 
+from time import sleep
+from random import randint
+velocidade = randint(79, 90)
+print('---' *10)
+print('CUIDADO, RADAR À FRENTE!\nLimite de velocidade 80km/h')
+print('---' *10)
+print('Verificando velocidade do veículo...')
+print('---' *10)
+sleep(3)
+print('A velocidade registrada foi de {}Km/h'.format(velocidade))
+if velocidade > 80:
+    multa = (velocidade-80) * 7
+    print('Excedeu o limite de velocidade de 80km/h\nVocê foi multado em R${:.2f}'.format(multa))
+else:
+    print('Tenha um bom dia! \nDirija com segurança!')
