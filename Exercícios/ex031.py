7f665d829a9da6c42eb8441f5509eb80eb2c3505
Darla Souza <144633945+Darlan03@users.noreply.github.com>
2023-10-08 00:56:17 -0300

    Update ex031.py
    exercicio 31 realizado

--- Exercícios/ex031.py
+++ Exercícios/ex031.py
@@ -1,2 +1,9 @@
-# Desenvolva um programa que pergunte a distância de uma viagem em Km. Calccule o preço da passagem, cobrando R$ 0,50
+# Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$ 0,50
 # por Km para viagens de até 200Km e R$ 0,45 para viagens mais longas.
+
+vig = int(input('Qual a distância em km da sua viagem? '))
+if vig <= 200:
+    preco = vig * 0.50
+else:
+    preco = vig * 0.45
+print('Sua viagem custará R${:.2f}'.format(preco))
