9e82b49e2e1a0f6ca2fada3a84c4deda28998850
Darla Souza <144633945+Darlan03@users.noreply.github.com>
2023-10-07 23:53:59 -0300

    Update ex030.py
    exercicio 30 realizado

--- Exercícios/ex030.py
+++ Exercícios/ex030.py
@@ -1,1 +1,8 @@
 # Crie um programa que leia um número inteiro e mostre na tela se ele é par ou ímpar.
+
+num = int(input('Digite um número e direi se ele é ímpar ou par: '))
+resto = num % 2
+if resto == 1:
+    print('Este número é ímpar!')
+else:
+    print('Este número é par!')
