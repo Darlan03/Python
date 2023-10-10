fdbb78bda59025955c6acc149086f5d2d97ef1b8
Darla Souza <darlan.as@outlook.com>
2023-10-10 10:33:59 -0300

    exercicio 43 atualizado linhas 4 e 5

--- Exercícios/ex043.py
+++ Exercícios/ex043.py
@@ -1,8 +1,8 @@
 # Desenvolva uma lógia que leia o peso e a altura de uma pessoa.
 # calcule o seu IMC e mostre seu status de acordo com a tablea abaixo
 
-peso = float(input('Qual é seu peso? (Kg)'))
-altura = float(input('Qual é sua altura? (m) '))
+peso = float(input('Qual é seu peso (Kg)? '))
+altura = float(input('Qual é sua altura (m)? '))
 imc = peso / (altura ** 2)
 print('O seu IMC é de {:.1f}'.format(imc))
 if imc < 18.5:
