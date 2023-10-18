6c00a7212921dbc363c5f979b102570f908f7b34
Darla Souza <darlan.as@outlook.com>
2023-10-18 11:53:54 -0300

    Novos exercícios resolvidos.

--- Exercícios/ex033.py
+++ Exercícios/ex033.py
@@ -1,1 +1,17 @@
 # Faça um programa que leia três números e mostre qual é maior e qual é menor.
+
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+c = int(input('Terceiro valor: '))
+menor = a
+if b < c and b < a:
+    menor = b
+if c < b and c < a:
+    menor = c
+maior = a
+if b > c and b > a:
+    maior = b
+if c > b and c > a:
+    maior = c
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
--- Exercícios/ex034.py
+++ Exercícios/ex034.py
@@ -1,3 +1,13 @@
 # Faça um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
 # Para salários superiores a R$ 1.250,00, calcule um aumento de 10%.
 # Para os inferiores ou iguais o aumento é de 15%.
+
+print('-=-' * 8)
+print('AUMENTO DE SALÁRIO!!!')
+print('-=-' * 8)
+sal = float(input('Qual o seu salário atual? '))
+if sal > 1250:
+    aum = sal + (sal * 10)/100
+else:
+    aum = sal + (sal * 15)/100
+print('Parabéns, seu novo salário passou a ser R${:.2f}'.format(aum))
