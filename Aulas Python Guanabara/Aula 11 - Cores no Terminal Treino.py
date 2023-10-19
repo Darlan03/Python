ea5d30980d33a33422543c6f76a998f0ab88581c
Darla Souza <darlan.as@outlook.com>
2023-10-19 16:19:20 -0300

    Renomeando as aulas

---
+++ Aulas Python Guanabara/Aula 11 - Cores no Terminal Treino.py
@@ -0,0 +1,6 @@
+# print('\033[1;30;43mOla, mundo!\033[m')
+a = float(input('Quantos anos você tem? '))
+# print('\033[30;41m{}\033[m'.format(a))
+
+# print('A minha idade é {}{}{}'.format('\033[1;30;41m', a, '\033[m'))
+print('\033[36m{}\033[m'.format(a))
---
+++ Aulas Python Guanabara/Aula 11 - Cores no Terminal.py
@@ -0,0 +1,23 @@
+# Colocando cores no Python com padrão ANSI escape sequence
+# Deve colocar \033[ para representação de cor
+# Dentro das chaves coloca o estilo (style), texto (text) e o fundo (back)
+# São separados por ';' e com 'm' no final
+
+# Estilo cod
+# 0 não tem estilo
+# 1 Negrito (bold)
+# 4 Sublinhado (underline)
+# 7 Negativo (inverte as cores)
+
+# Cores do texto
+# 30 - Branco
+# 31 - Vermelho
+# 32 - Verde
+# 33 - Amarelo
+# 34 - Azul
+# 35 - Roxo
+# 36 - Ciano
+# 37 - Cinza
+
+# Fundo (Back)
+# são as mesmas cores, porém os códigos são de 40 a 47
