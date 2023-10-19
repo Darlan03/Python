8535df40dbe9a743dc0b577bf552180a9dfeaeab
Darla Souza <darlan.as@outlook.com>
2023-10-19 14:31:33 -0300

    Exercício 35 resolvido

--- Exercícios/ex035.py
+++ Exercícios/ex035.py
@@ -1,3 +1,15 @@
 # Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
 
+print('=-=-' * 6)
+print('ANALISADOR DE TRIÂNGULOS')
+print('=-=-' * 6)
 
+print('Informe três segmento e direi se esses comprimentos podem formar um triângulo!')
+
+a = float(input('Primeiro segmento: '))
+b = float(input('Segundo segmento: '))
+c = float(input('Terceiro segmento: '))
+if a <= b + c and b <= a + c and c <= b + a:
+    print('Os segmentos PODEM formar um triângulo')
+else:
+    print('Os segmentos NÃO podem formar um triângulo')
