439e0c5bb1d9f410a54bb347bdb2e6b9e35203c0
Darla Souza <darlan.as@outlook.com>
2023-12-07 19:57:03 -0300

    Ex044 incompleto

--- Exercícios/ex044.py
+++ Exercícios/ex044.py
@@ -5,3 +5,23 @@
 # Em até 2x no cartão: preço normal
 # 3x ou mais no cartão: 20% de juros
 
+pdt = float(input('Qual o valor do produto? R$'))
+print("""Esolha a forma de pegamento:
+[1] À vista em dinheiro ou cheque terá 10% de desconto!
+[2] À vista no cartão terá 5% de desconto!
+[3] Em até 2x no cartão s/ juros!
+[4] 3x ou mais terá 20% de juros!""")
+opção = int(input('Escolha a forma de pegamento: '))
+if opção == 1:
+    vista = pdt - ((pdt * 10)/100)
+    print('O produto à vista sai a R${:.2f}'.format(vista))
+elif opção == 2:
+    crt5 = pdt - ((pdt * 5)/100)
+    print('O produto com 5% de desconto sai a R${:.2f}'.format(crt5))
+elif opção == 3:
+    crt2x = pdt/2
+    print('O produto em 2x s/ juros sai a R${:.2f} na primeira parcela e R${:.2f} na segunda parcela'.format(crt2x, crt2x))
+elif opção == 4
+    crt3x
+
+
