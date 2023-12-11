b425284e5d7f46ed2f0cb3c51f7c6fb90df8a572
Darla Souza <darlan.as@outlook.com>
2023-12-10 22:38:08 -0300

    Ex045 incompleto

--- Exercícios/ex044.py
+++ Exercícios/ex044.py
@@ -5,23 +5,26 @@
 # Em até 2x no cartão: preço normal
 # 3x ou mais no cartão: 20% de juros
 
-pdt = float(input('Qual o valor do produto? R$'))
+pdt = float(input('Qual o valor da sua compra? R$ '))
 print("""Esolha a forma de pegamento:
-[1] À vista em dinheiro ou cheque terá 10% de desconto!
+[1] À vista em dinheiro/cheque terá 10% de desconto!
 [2] À vista no cartão terá 5% de desconto!
 [3] Em até 2x no cartão s/ juros!
 [4] 3x ou mais terá 20% de juros!""")
 opção = int(input('Escolha a forma de pegamento: '))
 if opção == 1:
     vista = pdt - ((pdt * 10)/100)
-    print('O produto à vista sai a R${:.2f}'.format(vista))
+    print('Sua compra à vista sai a R${:.2f}'.format(vista))
 elif opção == 2:
     crt5 = pdt - ((pdt * 5)/100)
-    print('O produto com 5% de desconto sai a R${:.2f}'.format(crt5))
+    print('Sua compra com 5% de desconto sai a R${:.2f}'.format(crt5))
 elif opção == 3:
     crt2x = pdt/2
-    print('O produto em 2x s/ juros sai a R${:.2f} na primeira parcela e R${:.2f} na segunda parcela'.format(crt2x, crt2x))
-elif opção == 4
-    crt3x
-
-
+    print('Sua compra foi parcelada em 2x de R${:.2f} s/ juros'.format(crt2x))
+elif opção == 4:
+    total = pdt + (pdt * 20 / 100)
+    parc = int(input('Em quantas vezes deseja parcelar sua compra: '))
+    total1 = total / parc
+    print('Sua compra foi parcelada em {}x de R${:.2f}'.format(parc, total1))
+else:
+    print('Opção de pagamento inválida. Tente novente!')
--- Exercícios/ex045.py
+++ Exercícios/ex045.py
@@ -1,1 +1,15 @@
 # Crie um programa que faça o pc jogar jokenpô com você
+
+from random import randint
+itens = ('Pedra', 'Papel', 'Tesoura')
+pc = randint(0, 2)
+print('VAMOS JOGAR JOKENPÔ')
+j1 = int(input("""[0] Pedra
+[1] Papel
+[2] Tesoura
+Faça sua jogada: """))
+print('-=' * 10)
+print('Você escolheu {}'.format(itens[j1]))
+print('PC escolheu {}'.format(itens[pc]))
+print('-=' * 10)
+
